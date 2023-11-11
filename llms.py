6b95b1dde047bbab8a53b2160e32150299e7fd60
Metaverseforpc <57561743+vaibhavard@users.noreply.github.com>
2023-11-11 23:08:23 +0530

    Update llms.py

--- llms.py
+++ llms.py
@@ -1,6 +1,37 @@
 from functions import *
 import helpers.helper as helper
 import threading
+import asyncio
+async def run_provider(provider: helper.g4f.Provider.BaseProvider,messages):
+    try:
+        response = await helper.g4f.ChatCompletion.create_async(
+            model=helper.g4f.models.default,
+            messages=messages,
+            provider=provider,
+        )
+        print(f"{provider.__name__}:", response)
+        if response is not None and response != "" and "support@chatbase.co" not in response:
+            helper.providers.append(provider)
+
+            return response
+        else:
+            return None
+    except Exception as e:
+        return None
+        
+async def run_all():
+    calls = [
+        run_provider(provider,[{"role": "user", "content": "Hello!"}]) for provider in helper.provi
+    ]
+    for future in asyncio.as_completed(calls):
+        response = await future
+        if response is not None and response != "" and "support@chatbase.co" not in response:
+            print("✅")
+    
+def get_providers():
+    asyncio.run(run_all())
+    return str(helper.providers)
+
 
 def gpt4(messages,model="gpt-4"):
     print("GPT$ NO STREAM")
@@ -11,7 +42,9 @@ def gpt4(messages,model="gpt-4"):
     if "gpt-4" in model:
 
         try:
+            helper.data["stream"]=False
             json_body=ask("","",helper.api_endpoint,helper.data)
+            helper.data["stream"]=True
             helper.data['parentMessageId'] = json_body['messageId']
             return json_body['response']
         
