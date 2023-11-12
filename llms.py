c9a16d39d8407dac85f19d1799138866418e5fa4
Metaverseforpc <57561743+vaibhavard@users.noreply.github.com>
2023-11-12 10:21:46 +0530

    Update llms.py

--- llms.py
+++ llms.py
@@ -2,6 +2,7 @@ from functions import *
 import helpers.helper as helper
 import threading
 import asyncio
+
 async def run_provider(provider: helper.g4f.Provider.BaseProvider,messages):
     try:
         response = await helper.g4f.ChatCompletion.create_async(
@@ -32,24 +33,26 @@ def get_providers():
     asyncio.run(run_all())
     return str(helper.providers)
 
-
 def gpt4(messages,model="gpt-4"):
     print("GPT$ NO STREAM")
 
     if "gpt-3" in check(helper.api_endpoint):
         model="gpt-3"
 
-    if "gpt-4" in model:
+    if "gpt-4" in model :
 
         try:
             helper.data["stream"]=False
             json_body=ask("","",helper.api_endpoint,helper.data)
             helper.data["stream"]=True
-            helper.data['parentMessageId'] = json_body['messageId']
+            if "gpt-4-dev" not in model:
+                helper.data['parentMessageId'] = json_body['messageId']
             return json_body['response']
         
         except Exception as e:
+            print(e)
             model="gpt-3"
+
 
     if "gpt-3" in model:
 
@@ -57,7 +60,7 @@ def gpt4(messages,model="gpt-4"):
             try:
 
                 response = helper.g4f.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
+                    model="gpt-3.5-turbo",provider=provider ,
                     messages=messages,
                     stream=False)
                 print(response)
@@ -72,20 +75,22 @@ def gpt4(messages,model="gpt-4"):
 def gpt4stream(messages,model):
     print(f"-------{model}--------")
 
-    if "gpt-3" in check(helper.api_endpoint) and not "Bard" in model and not "llama" in model:
+    if "gpt-3" in check(helper.api_endpoint) and not "Bard" in model and not "llama" in model and not "gpt-4-alt" in model:
         model="gpt-3"
-        #helper.q.put("> Falling back to gpt-3\n\n") 
+        helper.q.put("> Falling back to gpt-3\n\n") 
 
 
-    if "gpt-4" in model and not "gpt-4-alt" in model:
+    if "gpt-4" in model :
         try:
-            print(helper.data["systemMessage"])
+            print(helper.data)
           
             with requests.post(helper.api_endpoint, json=helper.data, stream=True) as resp:
                 for line in resp.iter_lines():
                     if helper.stopped:
                         helper.stopped=False
                         print("TERMINATING..")
+                        helper.q.put("END") # mark the task as done
+
                         break
 
                     if line and "result" not in line.decode() and "conversationId" not in line.decode() and "[DONE]" not in line.decode():
@@ -95,7 +100,7 @@ def gpt4stream(messages,model):
                         try:
                             parsed_data = json.loads("{" + line.replace(":", ": ").replace("data", "\"data\"",1) + "}")
                         except Exception as e:
-                            parsed_data={"data":""}
+                            parsed_data={"data":"."}
                             model="gpt-3"
                             print(e)
                             ee=str(e)
@@ -105,60 +110,45 @@ def gpt4stream(messages,model):
                             helper.q.put(msg) 
 
                     elif line and "conversationId"  in line.decode():
+                        if "gpt-4-dev" not in model:
 
-                        json_body = line.decode().replace("data: ","")
-                        json_body = json.loads(json_body)
+                            json_body = line.decode().replace("data: ","")
+                            json_body = json.loads(json_body)
+                            try:
+                                ss = json_body["details"]["adaptiveCards"][0]["body"][1]["text"].replace(")","")
+                                links = extract_links(ss)
+                                para="\n\n"
+                                x=0
+                                for lnk in links:
+                                    x=x+1
+                                    para=para+f"""[^{x}^]: {lnk}
+                                    
+    """
+                                a="Links:"
+                                for i in range(1,x+1):
+                                    a = a + f"""[^{i}^]"""
+                                msg="\n\n\n"+a+para
+                                helper.q.put(msg) 
+
+                            except Exception as e:
+                                print(e)
+                                pass
+
                         try:
-                            ss = json_body["details"]["adaptiveCards"][0]["body"][1]["text"].replace(")","")
-                            links = extract_links(ss)
-                            para="\n\n"
-                            x=0
-                            for lnk in links:
-                                x=x+1
-                                para=para+f"""[^{x}^]: {lnk}
-                                
-"""
-                            a="Links:"
-                            for i in range(1,x+1):
-                                a = a + f"""[^{i}^]"""
-                            msg="\n\n\n"+a+para
-                            helper.q.put(msg) 
-
-                        except Exception as e:
-                            print(e)
-                            pass
-
-                        try:
-                            helper.data['parentMessageId'] = json_body['messageId']
+                            if "gpt-4-dev" not in model:
+                                helper.data['parentMessageId'] = json_body['messageId']
+                                print("Conversation history saved")
                         except:
                             pass
-                        
-                            
 
-                        print("Conversation history saved")
-                        helper.time.sleep(2)
                         helper.q.put("END") # mark the task as done
 
 
         except:
             model="gpt-3"
-            #helper.q.put("> Falling back to gpt-3\n\n")
-    if "gpt-4-alt" in model:
-        response = helper.g4f.ChatCompletion.create(
-            model=helper.g4f.models.default,
-            messages=messages,
-            provider=helper.g4f.Provider.Bing,
-            #cookies=g4f.get_cookies(".google.com"),
-            # cookies={"cookie_name": "value", "cookie_name2": "value2"},
-            stream=True
-        )
-        try:
-            for message in response:
-                helper.q.put(message)
-        except Exception as e:
-                helper.q.put(f"\nError:{e}")
-        helper.q.put("END") # mark the task as done
-        
+            helper.q.put("> Falling back to gpt-3\n\n")
+
+    
     if "gpt-3" in model:
 
         for provider in helper.providers:
