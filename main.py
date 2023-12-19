5aaa9bef77f5ae527f9ea4368645b1de2cff434c
Metaverseforpc <57561743+vaibhavard@users.noreply.github.com>
2023-12-19 19:26:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,13 +21,11 @@ import subprocess
 import os
 UPLOAD_FOLDER = 'static'
  
-codebot=Codebot()
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/v1/chat/completions", methods=['POST'])
 def chat_completions2():
-    global codebot
     helper.stopped=True
 
     streaming = req.json.get('stream', False)
@@ -202,6 +200,7 @@ def chat_completions2():
     if  streaming and "/aigen" not in helper.data["message"] and model!="gpt-4-code": 
         return app.response_class(stream_response(messages,model), mimetype='text/event-stream')
     elif streaming and "/aigen" in helper.data["message"]  and model!="gpt-4-code":
+        codebot=Codebot()
         helper.task_query=helper.data["message"].replace("/aigen","")
 
         return app.response_class(aigen(model), mimetype='text/event-stream')
